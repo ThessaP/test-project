2d3ba60c2ccee3838b0adcc2e24199f2ad064784
Theresa Pham <theresa.pham@no.experis.com>
2023-06-29 14:58:38 +0200

    done

---
+++ HelloWorld/Models/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using HelloWorld.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelloWorld.Models;
+
+public class TodoContext : DbContext
+{
+    public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<TodoItem> TodoItems { get; set; } = null!;
+}
